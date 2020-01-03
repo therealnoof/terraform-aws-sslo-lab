f599eaf2d28d9505006a53e8f14e84717092958e
therealnoof <hd1912@gmail.com>
2020-01-02 20:32:25 -0500

    Create variable.tf
    adding the variable.tf file

---
+++ variable.tf
@@ -0,0 +1,9 @@
+#
+# This outputs to the console asking for the SSH key for the EC2 instances
+# You should have created a key pair in advance
+#
+variable "ec2_key_name" {
+  description = "AWS EC2 Key name for SSH access"
+  type        = string
+}
+
