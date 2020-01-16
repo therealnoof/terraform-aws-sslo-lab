6e403bb3367e6cf0bdb5b3c2bbe08a3847667e77
therealnoof <hd1912@gmail.com>
2020-01-16 11:48:37 -0500

    Update main.tf
    updated some variable references, changed networks in inspection zone to full /24 and add source/destination checks to false

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Provider Declared
 #
 provider "aws" {
-  region = var.region
+  region = "${var.region}"
   shared_credentials_file = "~/.aws/credentials"
 }
 
@@ -26,7 +26,8 @@ module "vpc" {
   name                 = format("%s-vpc-%s", local.prefix, random_id.id.hex)
   cidr                 = local.cidr
   azs                  = ["${var.az}"]
-  enable_nat_gateway   = true
+  enable_nat_gateway   = "true"
+  enable_dns_hostnames = "true"
 }
 
 #
@@ -37,7 +38,7 @@ resource "aws_route_table" "sslo-lab-route-table" {
   
     route {
     cidr_block          = "0.0.0.0/0"
-    gateway_id          = aws_internet_gateway.sslo-lab-igw.id 
+    gateway_id          = "${aws_internet_gateway.sslo-lab-igw.id}"  
   }
   tags = {
     Name = "sslo-lab-route-table"
@@ -48,18 +49,33 @@ resource "aws_route_table" "sslo-lab-route-table" {
 # Create the Route Table associations
 #
 resource "aws_route_table_association" "sslo-lab-route-table-association" {
-  subnet_id             = aws_subnet.jumpbox.id
-  route_table_id        = aws_route_table.sslo-lab-route-table.id
+  subnet_id             = "${aws_subnet.jumpbox.id}" 
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
 }
 
 resource "aws_route_table_association" "sslo-lab-route-table-association-1" {
-  subnet_id             = aws_subnet.jumpbox-to-mgmt.id
-  route_table_id        = aws_route_table.sslo-lab-route-table.id
+  subnet_id             = "${aws_subnet.jumpbox-to-mgmt.id}"
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
 }
 
 resource "aws_route_table_association" "sslo-lab-route-table-association-2" {
-  subnet_id             = aws_subnet.bigip-internal-to-webserver.id
-  route_table_id        = aws_route_table.sslo-lab-route-table.id
+  subnet_id             = "${aws_subnet.bigip-internal-to-webserver.id}"
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
+}
+
+resource "aws_route_table_association" "sslo-lab-route-table-association-3" {
+  subnet_id             = "${aws_subnet.jumpbox-internal-to-bigip-vips.id}"
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
+}
+
+resource "aws_route_table_association" "sslo-lab-route-table-association-4" {
+  subnet_id             = "${aws_subnet.inspection_out.id}"
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
+}
+
+resource "aws_route_table_association" "sslo-lab-route-table-association-5" {
+  subnet_id             = "${aws_subnet.inspection_in.id}"
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
 }
 
 #
@@ -67,7 +83,7 @@ resource "aws_route_table_association" "sslo-lab-route-table-association-2" {
 #
 resource "aws_main_route_table_association" "sslo-lab-main-route-table-association" {
   vpc_id                = module.vpc.vpc_id
-  route_table_id        = aws_route_table.sslo-lab-route-table.id
+  route_table_id        = "${aws_route_table.sslo-lab-route-table.id}"
 }
 
 
@@ -91,7 +107,7 @@ resource "aws_internet_gateway" "sslo-lab-igw" {
 resource "aws_subnet" "jumpbox" {
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.1.0/24"
-  availability_zone     = var.az
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-jumpbox"
     Group_Name = "sslo-lab-jumpbox"
@@ -104,7 +120,7 @@ resource "aws_subnet" "jumpbox" {
 resource "aws_subnet" "jumpbox-internal-to-bigip-vips" {
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.4.0/24"
-  availability_zone     = var.az
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-jumpbox-internal-to-bigip-vips"
     Group_Name = "sslo-lab-jumpbox-internal-to-bigip-vips"
@@ -115,7 +131,7 @@ resource "aws_subnet" "jumpbox-internal-to-bigip-vips" {
 # Create External(MGMT) Network Interface for Jumpbox
 #
 resource "aws_network_interface" "sslo-lab-jumpbox-external" {
-  subnet_id             = aws_subnet.jumpbox.id
+  subnet_id             = "${aws_subnet.jumpbox.id}"
   security_groups       = ["${aws_security_group.jumpbox_external.id}", "${aws_security_group.jumpbox_to_mgmt.id}", "${aws_security_group.jumpbox_to_bigip_vips.id}"]
   tags = {
     Name = "sslo-lab-external-interface-jumpbox"
@@ -126,7 +142,7 @@ resource "aws_network_interface" "sslo-lab-jumpbox-external" {
 # Create Internal Network (1st)Interface to access BIGIP MGMT for Jumbbox
 #
 resource "aws_network_interface" "sslo-lab-jumpbox-to-BIGIP-mgmt" {
-  subnet_id             = aws_subnet.jumpbox-to-mgmt.id
+  subnet_id             = "${aws_subnet.jumpbox-to-mgmt.id}"
   security_groups       = ["${aws_security_group.jumpbox_to_mgmt.id}"] 
   tags = {
     Name = "sslo-lab-jumpbox-to-mgmt"
@@ -137,7 +153,7 @@ resource "aws_network_interface" "sslo-lab-jumpbox-to-BIGIP-mgmt" {
 # Create Internal Network (2nd)Interface to access BIGIP vips
 #
 resource "aws_network_interface" "sslo-lab-jumpbox-to-BIGIP-vips" {
-  subnet_id             = aws_subnet.jumpbox-internal-to-bigip-vips.id
+  subnet_id             = "${aws_subnet.jumpbox-internal-to-bigip-vips.id}"
   security_groups       = ["${aws_security_group.jumpbox_to_bigip_vips.id}"] 
   tags = {
     Name = "sslo-lab-jumpbox-to-bigip-vips"
@@ -156,9 +172,9 @@ resource "aws_security_group" "jumpbox_external" {
   }
   ingress {
     # RDP (change to whatever ports you need)
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -182,9 +198,9 @@ resource "aws_security_group" "jumpbox_to_bigip_vips" {
   }
   ingress {
     # RDP (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -195,11 +211,12 @@ resource "aws_security_group" "jumpbox_to_bigip_vips" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
 #
 # Create EIP Association
 #
 resource "aws_eip_association" "eip" {
-  network_interface_id        = aws_network_interface.sslo-lab-jumpbox-external.id
+  network_interface_id        = "${aws_network_interface.sslo-lab-jumpbox-external.id}"
   allocation_id               = "eipalloc-723ff64f"
 }
 
@@ -208,24 +225,24 @@ resource "aws_eip_association" "eip" {
 #
 resource "aws_instance" "jumpbox" {
 
-  ami                         = "ami-bc89acdd"  
-  instance_type               = "m4.xlarge"
+  ami                         = "${var.jumpbox_ami}"  
+  instance_type               = "m5.2xlarge"
   key_name                    = var.ec2_key_name  
-  availability_zone           = var.az
-  depends_on                  = [aws_internet_gateway.sslo-lab-igw]
+  availability_zone           = "${var.az}"
+  depends_on                  = ["aws_internet_gateway.sslo-lab-igw"]
   tags = {
     Name = "sslo-lab-jumpbox"
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-jumpbox-to-BIGIP-mgmt.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-jumpbox-to-BIGIP-mgmt.id}"
     device_index              = 1
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-jumpbox-external.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-jumpbox-external.id}"
     device_index              = 0
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-jumpbox-to-BIGIP-vips.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-jumpbox-to-BIGIP-vips.id}"
     device_index              = 2
   }
 }
@@ -240,7 +257,7 @@ resource "aws_instance" "jumpbox" {
 resource "aws_subnet" "jumpbox-to-mgmt" {
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.2.0/24"
-  availability_zone     = var.az
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-jumpbox-to-mgmt"
     Group_Name = "sslo-lab-jumpbox-to-mgmt"
@@ -253,7 +270,7 @@ resource "aws_subnet" "jumpbox-to-mgmt" {
 resource "aws_subnet" "bigip-internal-to-webserver" {
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.3.0/24"
-  availability_zone     = var.az
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-bigip-internal-to-webserver"
     Group_Name = "sslo-lab-bigip-internal-to-webserver"
@@ -265,8 +282,9 @@ resource "aws_subnet" "bigip-internal-to-webserver" {
 #
 resource "aws_network_interface" "sslo-lab-jumpbox-to-mgmt" {
   count                 = var.bigip_count
-  subnet_id             = aws_subnet.jumpbox-to-mgmt.id
-  security_groups       = ["${aws_security_group.jumpbox_to_mgmt.id}","${aws_security_group.bigip_to_internal_webserver.id}" ]
+  source_dest_check     = "false"
+  subnet_id             = "${aws_subnet.jumpbox-to-mgmt.id}"
+  security_groups       = ["${aws_security_group.jumpbox_to_mgmt.id}","${aws_security_group.bigip_to_internal_webserver.id}","${aws_security_group.inspection_zone.id}","${aws_security_group.jumpbox_to_bigip_vips.id}"]
   tags = {
     Name = "sslo-lab-jumpbox-to-mgmt-interface"
   }
@@ -277,8 +295,9 @@ resource "aws_network_interface" "sslo-lab-jumpbox-to-mgmt" {
 #
 resource "aws_network_interface" "sslo-lab-bigip-external-vips" {
   count                 = var.bigip_count
+  source_dest_check     = "false"
   private_ips_count     = "1"
-  subnet_id             = aws_subnet.jumpbox-internal-to-bigip-vips.id
+  subnet_id             = "${aws_subnet.jumpbox-internal-to-bigip-vips.id}"
   security_groups       = ["${aws_security_group.jumpbox_to_bigip_vips.id}"]
   tags = {
     Name = "sslo-lab-bigip-external-vips"
@@ -290,7 +309,8 @@ resource "aws_network_interface" "sslo-lab-bigip-external-vips" {
 #
 resource "aws_network_interface" "sslo-lab-bigip-interal-to-webserver" {
   count                 = var.bigip_count
-  subnet_id             = aws_subnet.bigip-internal-to-webserver.id
+  source_dest_check     = "false"
+  subnet_id             = "${aws_subnet.bigip-internal-to-webserver.id}"
   security_groups       = ["${aws_security_group.bigip_to_internal_webserver.id}"]
   tags = {
     Name = "sslo-lab-bigip-to-internal-webserver"
@@ -303,7 +323,8 @@ resource "aws_network_interface" "sslo-lab-bigip-interal-to-webserver" {
 #
 resource "aws_network_interface" "sslo-lab-bigip-inspection-out" {
   count                 = var.bigip_count
-  subnet_id             = aws_subnet.inspection_out.id
+  source_dest_check     = "false"
+  subnet_id             = "${aws_subnet.inspection_out.id}"
   security_groups       = ["${aws_security_group.inspection_zone.id}"]
   tags = {
     Name = "sslo-lab-firewall-bigip-inspection-out"
@@ -315,7 +336,8 @@ resource "aws_network_interface" "sslo-lab-bigip-inspection-out" {
 #
 resource "aws_network_interface" "sslo-lab-bigip-inspection-in" {
   count                 = var.bigip_count
-  subnet_id             = aws_subnet.inspection_in.id
+  source_dest_check     = "false"
+  subnet_id             = "${aws_subnet.inspection_in.id}"
   security_groups       = ["${aws_security_group.inspection_zone.id}"]
   tags = {
     Name = "sslo-lab-firewall-bigip-inspection-in"
@@ -334,9 +356,9 @@ resource "aws_security_group" "bigip_to_internal_webserver" {
   }
   ingress {
     # SSH (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -360,26 +382,11 @@ resource "aws_security_group" "jumpbox_to_mgmt" {
   }
   ingress {
     # SSH (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    # SSH (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    # SSH (change to whatever ports you need)
-    from_port   = 8443
-    to_port     = 8443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port       = 0
     to_port         = 0
@@ -395,10 +402,14 @@ resource "aws_instance" "bigip" {
 
   count                       = var.bigip_count
   ami                         = "ami-14520975"  
-  instance_type               = "m4.4xlarge"
-  key_name                    = var.ec2_key_name  
-  availability_zone           = var.az
-  depends_on                  = [aws_internet_gateway.sslo-lab-igw]
+  instance_type               = "m5.4xlarge"
+  key_name                    = var.ec2_key_name 
+  ebs_block_device {
+    volume_size               = 300
+    device_name               ="/dev/xvda"
+  } 
+  availability_zone           = "${var.az}"
+  depends_on                  = ["aws_internet_gateway.sslo-lab-igw"]
   tags = {
     Name = "sslo-lab-bigip"
   }
@@ -462,7 +473,8 @@ resource "aws_instance" "bigip" {
 #
 resource "aws_network_interface" "sslo-lab-webserver" {
   private_ips            = ["10.0.3.50"]
-  subnet_id             = aws_subnet.bigip-internal-to-webserver.id
+  source_dest_check     = "false"
+  subnet_id             = "${aws_subnet.bigip-internal-to-webserver.id}"
   security_groups       = ["${aws_security_group.bigip_to_internal_webserver.id}"]
   tags = {
     Name = "sslo-lab-webserver"
@@ -478,13 +490,13 @@ resource "aws_instance" "web-server" {
   ami                         = "ami-443f6525"  
   instance_type               = "t3a.small"
   key_name                    = var.ec2_key_name  
-  availability_zone           = var.az
-  depends_on                  = [aws_internet_gateway.sslo-lab-igw]
+  availability_zone           = "${var.az}"
+  depends_on                  = ["aws_internet_gateway.sslo-lab-igw"]
   tags = {
     Name = "sslo-lab-web-server"
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-webserver.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-webserver.id}"
     device_index              = 0
   }
 }
@@ -494,12 +506,12 @@ resource "aws_instance" "web-server" {
 ###########################
 
 #
-# Create Inspection Subnet out from BIG-IP to Palo 10.0.5.0/25
+# Create Inspection Subnet out from BIG-IP to Palo 10.0.5.0/24
 #
 resource "aws_subnet" "inspection_out" {
   vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.5.0/25"
-  availability_zone     = var.az
+  cidr_block            = "10.0.5.0/24"
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-inspection-out"
     Group_Name = "sslo-lab-inspection-out"
@@ -507,12 +519,12 @@ resource "aws_subnet" "inspection_out" {
 }
 
 #
-# Create Inspection Subnet in from Palo to BIG-IP 10.0.5.128/25
+# Create Inspection Subnet in from Palo to BIG-IP 10.0.6.0/24
 #
 resource "aws_subnet" "inspection_in" {
   vpc_id                = module.vpc.vpc_id
-  cidr_block            = "10.0.5.128/25"
-  availability_zone     = var.az
+  cidr_block            = "10.0.6.0/24"
+  availability_zone     = "${var.az}"
   tags = {
     Name = "sslo-lab-inspection-in"
     Group_Name = "sslo-lab-inspection-in"
@@ -523,9 +535,10 @@ resource "aws_subnet" "inspection_in" {
 # Create External(MGMT) Network Interface for Firewall
 #
 resource "aws_network_interface" "sslo-lab-firewall-mgmt" {
-  subnet_id             = aws_subnet.jumpbox-to-mgmt.id
-  private_ips            = ["10.0.2.50"]
-  security_groups       = ["${aws_security_group.jumpbox_to_mgmt.id}"]
+  subnet_id             = "${aws_subnet.jumpbox-to-mgmt.id}"
+  source_dest_check     = "false"
+  private_ips           = ["10.0.2.50"]
+  security_groups       = ["${aws_security_group.jumpbox_to_mgmt.id}","${aws_security_group.inspection_zone.id}"]
   tags = {
     Name = "sslo-lab-firewall-mgmt"
   }
@@ -535,8 +548,9 @@ resource "aws_network_interface" "sslo-lab-firewall-mgmt" {
 # Create Inspection Zone Network Interface from BIGIP to Firewall
 #
 resource "aws_network_interface" "sslo-lab-firewall-inspection-in" {
-  subnet_id             = aws_subnet.inspection_out.id
-  private_ips            = ["10.0.5.50"]
+  subnet_id             = "${aws_subnet.inspection_out.id}"
+  source_dest_check     = "false"
+  private_ips           = ["10.0.5.50"]
   security_groups       = ["${aws_security_group.inspection_zone.id}"]
   tags = {
     Name = "sslo-lab-firewall-firewall-inspection-in"
@@ -547,8 +561,9 @@ resource "aws_network_interface" "sslo-lab-firewall-inspection-in" {
 # Create Inspection Zone Network Interface from Palo to BIG-IP
 #
 resource "aws_network_interface" "sslo-lab-firewall-inspection-out" {
-  subnet_id             = aws_subnet.inspection_in.id
-  private_ips            = ["10.0.5.150"]
+  subnet_id             = "${aws_subnet.inspection_in.id}"
+  source_dest_check     = "false"
+  private_ips           = ["10.0.6.150"]
   security_groups       = ["${aws_security_group.inspection_zone.id}"]
   tags = {
     Name = "sslo-lab-firewall-firewall-inspection-out"
@@ -566,10 +581,10 @@ resource "aws_security_group" "inspection_zone" {
     Name = "sslo-lab-sg-inspection_zone"
   }
   ingress {
-    # SSH (change to whatever ports you need)
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    # Allow All (change to whatever ports you need)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -587,24 +602,24 @@ resource "aws_security_group" "inspection_zone" {
 resource "aws_instance" "firewall" {
 
   count                       = 1
-  ami                         = "ami-15547074"  
-  instance_type               = "m4.xlarge"
+  ami                         = "${var.paloalto_ami}"  
+  instance_type               = "m5.xlarge"
   key_name                    = var.ec2_key_name  
-  availability_zone           = var.az
-  depends_on                  = [aws_internet_gateway.sslo-lab-igw]
+  availability_zone           = "${var.az}"
+  depends_on                  = ["aws_internet_gateway.sslo-lab-igw"]
   tags = {
     Name = "sslo-lab-firewall"
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-firewall-mgmt.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-firewall-mgmt.id}"
     device_index              = 0
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-firewall-inspection-in.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-firewall-inspection-in.id}"
     device_index              = 1
   }
   network_interface {
-    network_interface_id      = aws_network_interface.sslo-lab-firewall-inspection-out.id
+    network_interface_id      = "${aws_network_interface.sslo-lab-firewall-inspection-out.id}"
     device_index              = 2
   }
 }
